020c0f31d1e4894b563dbb660f252e94d3c5675c
freeaoi <bluefyrezgaming@gmail.com>
2023-10-16 22:48:54 -0600

    ✨ feat: environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { AuthenticationModule } from './modules/authentication/authentication.module';
+import { UsuarioModule } from './modules/usuario/usuario.module';
+import { DatabaseModule } from './providers/database/database.module';
 
 @Module({
-    imports: [],
+    imports: [DatabaseModule, UsuarioModule, AuthenticationModule],
     controllers: [AppController],
     providers: [],
 })
