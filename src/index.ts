3795a70489961249bcff139ad7cd78c82acc5bab
freeaoi <bluefyrezgaming@gmail.com>
2023-10-20 02:30:22 -0600

    feat: docs, validations and serializations

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
 import { config } from 'dotenv';
 config();
 
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            transform: true,
+            stopAtFirstError: true,
+        })
+    );
+
+    app.useGlobalInterceptors(
+        new ClassSerializerInterceptor(app.get(Reflector), {
+            excludeExtraneousValues: true,
+        })
+    );
+
+    const config = new DocumentBuilder()
+        .setTitle('BienestarNIC API')
+        .setDescription('The BienestarNIC API documentation')
+        .addBearerAuth()
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+
     await app.listen(3000);
 }
 
