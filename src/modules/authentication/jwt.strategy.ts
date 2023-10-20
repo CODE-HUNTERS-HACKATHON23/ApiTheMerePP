6f49cffe4844177df53546dbb6b85e6399d8a01b
freeaoi <bluefyrezgaming@gmail.com>
2023-10-20 15:26:57 -0600

    fix: correctly dto type

--- src/modules/authentication/jwt.strategy.ts
+++ src/modules/authentication/jwt.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JWTPayloadDTO } from './dto/jwtPayload.dto';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
@@ -11,7 +12,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
         });
     }
 
-    async validate(payload: any) {
+    async validate(payload: JWTPayloadDTO) {
         return payload;
     }
 }
