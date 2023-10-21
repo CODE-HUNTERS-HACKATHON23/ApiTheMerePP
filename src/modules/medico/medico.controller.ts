ec8afe3086e4529da59c1a4715e249c8f16d171f
freeaoi <bluefyrezgaming@gmail.com>
2023-10-20 18:36:08 -0600

    feat: GET paciente by id endpoint

--- src/modules/medico/medico.controller.ts
+++ src/modules/medico/medico.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { CurrentUser } from '../../common/decorators/userCurrent.decorator';
+import { JWTPayloadDTO } from '../authentication/dto/jwtPayload.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { RoleGuard } from '../../common/guards/roles.guard';
+import { MedicoService } from './medico.service';
 
+@UseGuards(AuthGuard('jwt'), RoleGuard('Medico'))
 @Controller('medico')
-export class MedicoController {}
+export class MedicoController {
+    constructor(private readonly medicoService: MedicoService) {}
+    @Get('pacientes/:id')
+    getMedicoPacientes(
+        @CurrentUser() user: JWTPayloadDTO,
+        @Param('id') id: string
+    ) {
+        return this.medicoService.getMedicoPacientes({
+            idUsuario: user.idUsuario,
+            idPaciente: parseInt(id),
+        });
+    }
+}
--- src/modules/medico/medico.service.ts
+++ src/modules/medico/medico.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { DatabaseService } from '../../providers/database/database.service';
 
 @Injectable()
-export class MedicoService {}
+export class MedicoService {
+    constructor(private readonly databaseService: DatabaseService) {}
+    getMedicoPacientes(param: { idPaciente: number; idUsuario: number }) {
+        const medico = this.databaseService.medico.findUnique({
+            where: {
+                idUsuario: param.idUsuario,
+            },
+        });
+
+        if (!medico) throw new HttpException('El medico no existe', 404);
+
+        // return pacientes where there is almost one consulta with the medico
+        return this.databaseService.paciente.findMany({
+            where: {
+                consultas: {
+                    some: {
+                        medico: {
+                            idUsuario: param.idUsuario,
+                        },
+                    },
+                },
+            },
+        });
+    }
+}
